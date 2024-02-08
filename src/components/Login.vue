140df20e041f0d21f10290f2b3eb04c982f95a39
John Obelenus <jobelenus@gmail.com>
2024-02-08 11:34:50 -0500

    basic login html

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -18,5 +18,13 @@ function setUser() {
 </script>
 
 <template>
-    <p>Login here</p>
+    <div class="my-3 mx-3 mb-4 md:flex md:flex-wrap md:justify-between">
+        <!-- one downside of left/right forms is the baseline alignment of the label and input... after some fiddling with tailwind, its pretty close -->
+        <!-- i might recommend changing to a "label on top of the form" layout -->
+        <label for="username" class="flex leading-10 flex-col pr-2 md:w-1/3 text-right">Username</label>
+        <input @keyup.enter="setUser" v-model="username" id="username" class="flex leading-3 leading-tight flex-col pl-2 md:w-2/3 appearance-none focus:outline-none focus:shadow-outline" type="text" placeholder="Person Doe">
+    </div>
+    <div class="mb-4 md:w-full text-right">
+        <button class="mx-2" @click="setUser">Login</button>
+    </div>
 </template>
