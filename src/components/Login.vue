14e95648b6b51a0ec22ec92568ae0a970db4328d
John Obelenus <jobelenus@gmail.com>
2024-02-08 11:26:25 -0500

    rough sketch of the login logic

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,20 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const username = ref('')
+
+function setUser() {
+    if (username.value) {
+        // send to the back end
+        // set in a store
+        router.push('/chat')
+    } else {
+        alert('Please enter a username') // TODO replace with error in the UI
+    }
+}
 </script>
 
 <template>
