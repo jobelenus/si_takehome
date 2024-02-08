e2d773c3b0ce62766e64035cae076bebed43ae19
John Obelenus <jobelenus@gmail.com>
2024-02-08 12:34:31 -0500

    swap whether we are centering, or stretching our flex items, based on the route...

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 
 <template>
-  <main class="flex min-h-screen justify-center px-[5%] items-center">
+  <main class="flex min-h-screen justify-center px-[5%]" :class="{'items-center': route.name=='home', 'items-stretch': route.name == 'chat' }">
     <RouterView />
   </main>
 </template>
