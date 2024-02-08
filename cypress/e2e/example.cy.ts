b1390ce3af239225f01ed4f1ab084ea391d88870
John Obelenus <jobelenus@gmail.com>
2024-02-08 14:06:45 -0500

    e2e tests for login protection, and ability to chat

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,8 +1,27 @@
 // https://on.cypress.io/api
 
-describe('My First Test', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h5', 'Welcome to System Initiative!')
+describe('loading & router', () => {
+    it('visits the app root url', () => {
+      cy.visit('/')
+      cy.contains('h5', 'Welcome')
+    })
+  
+    it('should not allow you to chat without username', () => {
+      cy.visit('/chat')
+      cy.location('pathname').should('eq', '/')
+      cy.contains('h5', 'Welcome')
+    })
+  
+    it('should let you chat after you supply a username', () => {
+      cy.visit('/')
+      cy.contains('h5', 'Welcome')
+      cy.get('#username').type('cypress test')
+      cy.get('button').click()
+      cy.location('pathname').should('eq', '/chat')
+      cy.get('input').type('hello from cypress')
+      cy.get('button').click()
+      cy.contains('li', 'cypress test')
+      cy.contains('li', 'hello from cypress')
+    })
   })
-})
+  
