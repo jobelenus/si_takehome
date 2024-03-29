470771cec01a23188ee8aee46571c4fc55f8b2e2
John Obelenus <jobelenus@systeminit.com>
2024-03-29 17:28:43 -0400

    this was a large part of my pain and mistake

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './assets/output.css'
 
 import { createApp, unref } from 'vue'
 import { createPinia } from 'pinia'
-import { VueQueryPlugin } from '@tanstack/vue-query'
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 
 
 import App from './App.vue'
@@ -16,6 +16,17 @@ app.use(VueQueryPlugin)
 app.use(router)
 
 const userStore = useUserStore()
+
+// APPARENTLY, I missed this... this is the key :facepalm:
+// I was init'ing this in the store, bad news bears
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+})
 
 router.beforeEach((to, from, next) => {
     // if you don't have a username in the store you're not logged in and cannot access the chat
